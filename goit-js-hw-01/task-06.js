62b2497dd86651cbbb9038680def1825efe87125
RomanPidlisnychyi <myLuckyhelper@gmail.com>
2020-04-19 14:40:33 +0300

    shorty

--- goit-js-hw-01/task-06.js
+++ goit-js-hw-01/task-06.js
@@ -5,7 +5,6 @@ do {
     if (input === null) {
         alert(`Общая сумма чисел равна ${total}`);
         break;
-    } else {
-        total += input = Number(input);
     }
+    total += input = Number(input);
 } while (input !== null);
