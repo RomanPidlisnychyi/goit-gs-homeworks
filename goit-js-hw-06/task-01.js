a90ea715fd5ed72d3f76645779f5b5f9ff408bb8
RomanPidlisnychyi <myLuckyhelper@gmail.com>
2020-05-27 07:09:55 +0300

    init

---
+++ goit-js-hw-06/task-01.js
@@ -0,0 +1,237 @@
+const users = [{
+        id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
+        name: 'Moore Hensley',
+        email: 'moorehensley@indexia.com',
+        eyeColor: 'blue',
+        friends: ['Sharron Pace'],
+        isActive: false,
+        balance: 2811,
+        skills: ['ipsum', 'lorem'],
+        gender: 'male',
+        age: 37,
+    },
+    {
+        id: '7a3cbd18-57a1-4534-8e12-1caad921bda1',
+        name: 'Sharlene Bush',
+        email: 'sharlenebush@tubesys.com',
+        eyeColor: 'blue',
+        friends: ['Briana Decker', 'Sharron Pace'],
+        isActive: true,
+        balance: 3821,
+        skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+        gender: 'female',
+        age: 34,
+    },
+    {
+        id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
+        name: 'Ross Vazquez',
+        email: 'rossvazquez@xinware.com',
+        eyeColor: 'green',
+        friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+        isActive: false,
+        balance: 3793,
+        skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+        gender: 'male',
+        age: 24,
+    },
+    {
+        id: '249b6175-5c30-44c6-b154-f120923736f5',
+        name: 'Elma Head',
+        email: 'elmahead@omatom.com',
+        eyeColor: 'green',
+        friends: ['Goldie Gentry', 'Aisha Tran'],
+        isActive: true,
+        balance: 2278,
+        skills: ['adipisicing', 'irure', 'velit'],
+        gender: 'female',
+        age: 21,
+    },
+    {
+        id: '334f8cb3-eb04-45e6-abf4-4935dd439b70',
+        name: 'Carey Barr',
+        email: 'careybarr@nurali.com',
+        eyeColor: 'blue',
+        friends: ['Jordan Sampson', 'Eddie Strong'],
+        isActive: true,
+        balance: 3951,
+        skills: ['ex', 'culpa', 'nostrud'],
+        gender: 'male',
+        age: 27,
+    },
+    {
+        id: '150b00fb-dd82-427d-9faf-2879ea87c695',
+        name: 'Blackburn Dotson',
+        email: 'blackburndotson@furnigeer.com',
+        eyeColor: 'brown',
+        friends: ['Jacklyn Lucas', 'Linda Chapman'],
+        isActive: false,
+        balance: 1498,
+        skills: ['non', 'amet', 'ipsum'],
+        gender: 'male',
+        age: 38,
+    },
+    {
+        id: 'e1bf46ab-7168-491e-925e-f01e21394812',
+        name: 'Sheree Anthony',
+        email: 'shereeanthony@kog.com',
+        eyeColor: 'brown',
+        friends: ['Goldie Gentry', 'Briana Decker'],
+        isActive: true,
+        balance: 2764,
+        skills: ['lorem', 'veniam', 'culpa'],
+        gender: 'female',
+        age: 39,
+    },
+];
+
+//      ----Задание 1----
+const getUserNames = users => {
+    const userName = [];
+    users.map(function(user) {
+        userName.push(user.name);
+    });
+    return userName;
+};
+
+console.log(getUserNames(users));
+
+//      ----Задание 2----
+const getUsersWithEyeColor = (users, color) => {
+    const needEyeColor = [];
+    users.filter(function(user) {
+        if (user.eyeColor === color) {
+            needEyeColor.push(user);
+        }
+    });
+
+    return needEyeColor;
+};
+
+console.log(getUsersWithEyeColor(users, 'blue'));
+
+//      ----Задание 3----
+const getUsersWithGender = (users, gender) => {
+    const userWithGender = [];
+
+    users.filter(function(user) {
+        if (user.gender === gender) {
+            userWithGender.push(user.name);
+        }
+    });
+
+    return userWithGender;
+};
+
+console.log(getUsersWithGender(users, 'male'));
+
+//      ----Задание 4----
+const getInactiveUsers = users => {
+    const inactiveUsers = [];
+
+    users.filter(function(user) {
+        if (!user.isActive) {
+            inactiveUsers.push(user);
+        }
+    });
+
+    return inactiveUsers;
+};
+
+console.log(getInactiveUsers(users));
+
+//      ----Задание 5----
+const getUserWithEmail = (users, email) => {
+    let findUserWithEmail;
+
+    users.find(function(user) {
+        if (user.email === email) {
+            findUserWithEmail = user;
+        }
+    });
+
+    return findUserWithEmail;
+};
+
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com'));
+console.log(getUserWithEmail(users, 'elmahead@omatom.com'));
+
+//      ----Задание 6----
+const getUsersWhithAge = (users, min, max) => {
+    const usersWithAge = [];
+
+    users.filter(function(user) {
+        if (user.age >= min && user.age <= max) {
+            usersWithAge.push(user);
+        }
+    });
+
+    return usersWithAge;
+};
+
+console.log(getUsersWhithAge(users, 20, 30));
+console.log(getUsersWhithAge(users, 30, 40));
+
+//      ----Задание 7----
+const calculateTotalBalance = users => {
+    let totalBalance;
+    users.reduce(function(acc, user) {
+        return (totalBalance = acc + user.balance);
+    }, 0);
+
+    return totalBalance;
+};
+
+console.log(calculateTotalBalance(users));
+
+//      ----Задание 8----
+const getUsersWhithFriend = (users, friendName) => {
+    const findUsersWithFriend = [];
+
+    users.filter(function(user) {
+        if (user.friends.includes(friendName)) {
+            findUsersWithFriend.push(user.name);
+        }
+    });
+
+    return findUsersWithFriend;
+};
+
+console.log(getUsersWhithFriend(users, 'Briana Decker'));
+console.log(getUsersWhithFriend(users, 'Goldie Gentry'));
+
+//      ----Задание 9----
+const getNamesSortedByFriendsCount = users => {
+    const namesSortedByFriendsCount = users
+        .sort(function(prevUser, nextUser) {
+            return prevUser.friends.length - nextUser.friends.length;
+        })
+        .map(function(user) {
+            return user.name;
+        });
+
+    return namesSortedByFriendsCount;
+};
+
+console.log(getNamesSortedByFriendsCount(users));
+
+//      ----Задание 10----
+const getSortedUniqueSkills = users => {
+    const sortedUniqueSkills = users
+        .reduce(function(acc, user) {
+            acc.push(...user.skills);
+
+            return acc;
+        }, [])
+        .reduce(function(acc, skill) {
+            if (!acc.includes(skill)) {
+                acc.push(skill);
+            }
+
+            return acc;
+        }, [])
+        .sort();
+
+    return sortedUniqueSkills;
+};
+
+console.log(getSortedUniqueSkills(users));
