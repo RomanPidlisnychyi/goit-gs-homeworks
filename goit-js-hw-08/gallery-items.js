b8dedeee19aa39960db94b651ddf263b5a0ef33e
RomanPidlisnychyi <myLuckyhelper@gmail.com>
2020-06-12 06:22:42 +0300

    fn

--- goit-js-hw-08/gallery-items.js
+++ goit-js-hw-08/gallery-items.js
@@ -2,55 +2,55 @@ const images = [{
         preview: 'https://cdn.pixabay.com/photo/2019/05/14/16/43/hokkaido-4202825__340.jpg',
         original: 'https://cdn.pixabay.com/photo/2019/05/14/16/43/hokkaido-4202825_1280.jpg',
         description: 'Hokkaido Flower',
-        index: 1,
+        index: 0,
     },
     {
         preview: 'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
         original: 'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
         description: 'Container Haulage Freight',
-        index: 2,
+        index: 1,
     },
     {
         preview: 'https://cdn.pixabay.com/photo/2019/05/16/09/47/view-4206785__340.jpg',
         original: 'https://cdn.pixabay.com/photo/2019/05/16/09/47/view-4206785_1280.jpg',
         description: 'Aerial Beach View',
-        index: 3,
+        index: 2,
     },
     {
         preview: 'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
         original: 'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
         description: 'Flower Blooms',
-        index: 4,
+        index: 3,
     },
     {
         preview: 'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
         original: 'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
         description: 'Alpine Mountains',
-        index: 5,
+        index: 4,
     },
     {
         preview: 'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
         original: 'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
         description: 'Mountain Lake Sailing',
-        index: 6,
+        index: 5,
     },
     {
         preview: 'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
         original: 'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
         description: 'Alpine Spring Meadows',
-        index: 7,
+        index: 6,
     },
     {
         preview: 'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
         original: 'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
         description: 'Nature Landscape',
-        index: 8,
+        index: 7,
     },
     {
         preview: 'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
         original: 'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
         description: 'Lighthouse Coast Sea',
-        index: 9,
+        index: 8,
     },
 ];
 
@@ -76,6 +76,7 @@ const galleryList = images.map(image => {
     img.classList.add('gallery__image');
     img.src = image.preview;
     img.dataset.source = image.original;
+    img.dataset.index = image.index;
     img.alt = image.description;
 
     a.appendChild(img);
@@ -112,20 +113,47 @@ function onModalClose() {
 }
 
 function listener(event) {
-    if (event.code !== 'Escape') {
-        return;
+    console.log(event.code);
+    if (event.code === 'ArrowLeft' && refs.modalImageRef.index > 0) {
+        prevImageOnGallery();
     }
-    removeModalClass();
-    onModalClose();
+    if (
+        event.code === 'ArrowRight' &&
+        refs.modalImageRef.index < images.length - 1
+    ) {
+        console.log(images.length);
+        nextImageOnGallery();
+    }
+    if (event.code === 'Escape') {
+        removeModalClass();
+        onModalClose();
+    }
 }
 
 function addModalClass() {
     const src = event.target.dataset.source;
+    const index = event.target.dataset.index;
     refs.modalRef.classList.add('is-open');
     refs.modalImageRef.src = src;
+    refs.modalImageRef.index = index;
 }
 
 function removeModalClass() {
     refs.modalRef.classList.remove('is-open');
     refs.modalImageRef.src = '';
+    refs.modalImageRef.index = '';
+}
+
+function prevImageOnGallery() {
+    const index = Number(refs.modalImageRef.index);
+    const prevImageSrc = images[index - 1].original;
+    refs.modalImageRef.src = prevImageSrc;
+    refs.modalImageRef.index = index - 1;
+}
+
+function nextImageOnGallery() {
+    const index = Number(refs.modalImageRef.index);
+    const nextImageSrc = images[index + 1].original;
+    refs.modalImageRef.src = nextImageSrc;
+    refs.modalImageRef.index = index + 1;
 }
