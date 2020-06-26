85a628d451bc1c8407266ea3204034d924b23477
RomanPidlisnychyi <myLuckyhelper@gmail.com>
2020-06-26 20:59:46 +0300

    init

---
+++ goit-js-hw-10/src/index.js
@@ -0,0 +1,47 @@
+import './styles.css';
+import menu from './menu.json';
+import menuTemplate from './menuTemplate.hbs';
+
+const markup = menuTemplate(menu);
+
+const refs = {
+    menuRef: document.querySelector('.js-menu'),
+    bodyRef: document.querySelector('body'),
+    switchInputRef: document.querySelector('.js-switch-input'),
+};
+
+const theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+
+refs.bodyRef.classList.add(localStorage.theme);
+
+savedThemeClass();
+
+refs.switchInputRef.addEventListener('change', themeSwitchHandler);
+
+function themeSwitchHandler(event) {
+    const defaultTheme = event.path[0].checked;
+    localStorage.setItem('theme', defaultTheme ? theme.DARK : theme.LIGHT);
+    addThemeClass();
+}
+
+function savedThemeClass() {
+    if (localStorage.theme === theme.DARK) {
+        refs.switchInputRef.checked = true;
+    }
+}
+
+function addThemeClass() {
+    if (localStorage.theme === theme.DARK) {
+        refs.bodyRef.classList.remove('light-theme');
+        refs.bodyRef.classList.add('dark-theme');
+    }
+    if (localStorage.theme === theme.LIGHT) {
+        refs.bodyRef.classList.remove('dark-theme');
+        refs.bodyRef.classList.add('light-theme');
+    }
+}
+
+refs.menuRef.insertAdjacentHTML('beforeend', markup);
