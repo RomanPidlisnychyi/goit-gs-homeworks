8833e74d882bdc5915f77533b485f34de513c873
RomanPidlisnychyi <myLuckyhelper@gmail.com>
2020-07-06 19:48:41 +0300

    update timer

--- goit-js-hw-11/timer/index.js
+++ goit-js-hw-11/timer/index.js
@@ -2,29 +2,41 @@ class CountdownTimer {
     constructor(selector, targetDate) {
         this.selector = selector;
         this.targetDate = targetDate;
+        this.days = Math.floor(this.genarateTime() / (1000 * 60 * 60 * 24));
+        this.hours = this.pad(
+            Math.floor(
+                (this.genarateTime() % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+            ),
+        );
+        this.mins = this.pad(
+            Math.floor((this.genarateTime() % (1000 * 60 * 60)) / (1000 * 60)),
+        );
+        this.secs = this.pad(
+            Math.floor((this.genarateTime() % (1000 * 60)) / 1000),
+        );
     }
     createTimerHTML() {
         return `<div class="timer" id="${this.selector}">
-        <div class="field">
-          <span class="value" data-value="days-${this.selector}">0</span>
-          <span class="label">Days</span>
-        </div>
-      
-        <div class="field">
-          <span class="value" data-value="hours-${this.selector}">00</span>
-          <span class="label">Hours</span>
-        </div>
-      
-        <div class="field">
-          <span class="value" data-value="mins-${this.selector}">00</span>
-          <span class="label">Minutes</span>
-        </div>
-      
-        <div class="field">
-          <span class="value" data-value="secs-${this.selector}">00</span>
-          <span class="label">Seconds</span>
-        </div>
-      </div>`;
+            <div class="field">
+            <span class="value" data-value="days-${this.selector}">${this.days}</span>
+              <span class="label">Days</span>
+            </div>
+
+            <div class="field">
+              <span class="value" data-value="hours-${this.selector}">${this.hours}</span>
+              <span class="label">Hours</span>
+            </div>
+
+            <div class="field">
+              <span class="value" data-value="mins-${this.selector}">${this.mins}</span>
+              <span class="label">Minutes</span>
+            </div>
+
+            <div class="field">
+              <span class="value" data-value="secs-${this.selector}">${this.secs}</span>
+              <span class="label">Seconds</span>
+            </div>
+          </div>`;
     }
     isertTimetToDOM() {
         const wrap = document.querySelector('.wrap');
@@ -34,20 +46,23 @@ class CountdownTimer {
         window.addEventListener('DOMContentLoaded', this.setTimer(this));
     }
 
+    genarateTime() {
+        const finishedTimerDate = Date.parse(this.targetDate);
+
+        const currentDate = Date.now();
+
+        const time = finishedTimerDate - currentDate;
+
+        if (finishedTimerDate < currentDate) {
+            return;
+        }
+        return time;
+    }
+
     setTimer() {
         this.isertTimetToDOM();
         setInterval(() => {
-            const finishedTimerDate = Date.parse(this.targetDate);
-
-            const currentDate = Date.now();
-
-            const time = finishedTimerDate - currentDate;
-
-            if (finishedTimerDate < currentDate) {
-                return;
-            }
-
-            this.apdateClockFace(time);
+            this.apdateClockFace(this.genarateTime());
         }, 1000);
     }
     apdateClockFace(time) {
